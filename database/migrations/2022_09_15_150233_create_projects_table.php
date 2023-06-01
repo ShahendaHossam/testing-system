90acfc2b00acdc9071a6afd85f2ac0749fcfd22e
Shahenda <shahenda.cs.99@gmail.com>
2023-06-01 09:07:23 +0200

    create project migration file

---
+++ database/migrations/2022_09_15_150233_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('title', 255)->nullable();
+            $table->string('description', 255)->nullable();
+            $table->timestamps();
+            $table->softDeletesTz('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
