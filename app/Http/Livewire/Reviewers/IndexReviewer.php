1e881d099a963eded6edc95ed9b5aab972a7cb68
Shahenda <shahenda.cs.99@gmail.com>
2023-06-01 08:54:33 +0200

    create the index reviewer component to display the stored data in database in a table and set render functions

---
+++ app/Http/Livewire/Reviewers/IndexReviewer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Livewire\Reviewers;
+
+use App\Models\Status;
+use App\Models\TestCase;
+use App\Models\TestCaseDataRequirement;
+use App\Models\TestCaseStep;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class IndexReviewer extends Component
+{
+    use WithPagination;
+    // protected $paginationTheme = 'bootstrap';
+    public $sortField = 'id';
+    public $sortDirection = 'asc';
+    public $searchTerm = '';
+    public $result_no = 20;
+    public $search = '';
+    public $statuses;
+
+
+
+    public function sortBy($field)
+    {
+        if($this->sortDirection == 'asc')
+        {
+            $this->sortDirection = 'desc';
+        }
+        else
+        {
+            $this->sortDirection = 'asc';
+        }
+        $this->sortField = $field;
+    }
+
+    
+    public function render()
+    {
+        $status = Status::where('id', 1)->first();
+        $this->statuses = $status?$status->id:null;
+        $testcases = TestCase::when($this->statuses, function ($query) {
+                $query->where('status_id', $this->statuses);
+                })->when($this->search, function ($searchquery) {
+                $searchquery->where(function ($queryx) {
+                $queryx->where('test_cases.id', 'like', '%' . $this->search . '%')
+                ->orWhere('test_cases.title', 'like', '%' . $this->search . '%')
+                ->orWhere('test_cases.project_id', 'like', '%' . $this->search . '%')   
+                ->orWhere('test_cases.module_name', 'like', '%' . $this->search . '%')   
+                ->orWhere('test_cases.description', 'like', '%' . $this->search . '%')   
+                ->orWhere('test_cases.priority_id', 'like', '%' . $this->search . '%')
+                ->orWhere('test_cases.designer_id', 'like', '%' . $this->search . '%')  
+                ->orWhere('test_cases.created_at', 'like', '%' . $this->search . '%')  
+                ->orWhere('test_cases.pre_condition', 'like', '%' . $this->search . '%'); 
+            });
+        })->orderBy($this->sortField, $this->sortDirection)->paginate($this->result_no);
+        return view('livewire.reviewers.index-reviewer',compact('testcases'));
+    }
+}
